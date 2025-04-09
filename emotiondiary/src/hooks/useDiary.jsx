390545a61aca6a770ee20d7b9099a3fa52e9b555
orinery <gucheahyun@gmail.com>
2025-04-09 19:14:52 +0900

    0409/수정기능, 다이어리보기

---
+++ emotiondiary/src/hooks/useDiary.jsx
@@ -0,0 +1,23 @@
+import { useContext, useState, useEffect } from "react";
+import { DiaryStateContext } from "../App";
+import { useNavigate } from "react-router-dom";
+
+const useDiary = (id) => {
+  const data = useContext(DiaryStateContext);
+  const [curDiaryItem, setCurDiaryItem] = useState();
+  const nav = useNavigate();
+
+  useEffect(() => {
+    const currentDiaryItem = data.find(
+      (item) => String(item.id) === String(id)
+    );
+    if (!currentDiaryItem) {
+      window.alert("일기가 존재하지 않습니다.");
+      nav("/", { replace: true });
+    }
+    setCurDiaryItem(currentDiaryItem);
+  }, [id]);
+  return curDiaryItem;
+};
+
+export default useDiary;
--- emotiondiary/src/pages/Diary.jsx
+++ emotiondiary/src/pages/Diary.jsx
@@ -1,5 +1,31 @@
+import { useParams, useNavigate } from "react-router-dom";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import useDiary from "../hooks/useDiary";
+import Viewer from "../components/Viewer";
+import { getStringedDate } from "../util/get_stringed_date";
+
 const Diary = () => {
-  return <div>this is Diary page</div>;
+  const params = useParams();
+  const nav = useNavigate();
+  const curDiaryItem = useDiary(params.id);
+  if (!curDiaryItem) {
+    return <div>로딩중...</div>;
+  }
+  const { createdDate, emotionId, content } = curDiaryItem;
+  const title = getStringedDate(new Date(createdDate));
+  return (
+    <div>
+      <Header
+        title={`${title} 기록`}
+        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로가기"} />}
+        rightChild={
+          <Button onClick={() => nav(`/edit/${params.id}`)} text={"수정하기"} />
+        }
+      />
+      <Viewer emotionId={emotionId} content={content} />
+    </div>
+  );
 };
 
 export default Diary;
